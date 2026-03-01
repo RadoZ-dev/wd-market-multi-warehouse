93c396175d302376d7e3bb83ca631b591894b056
RadoZ-dev <rado.jazzfusion@gmail.com>
2026-03-01 17:54:22 +0100

    DAY_IN_SECONDS - define missing wp constant into test environment

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,3 +3,8 @@
 declare(strict_types=1);
 
 require_once dirname( __DIR__ ) . '/vendor/autoload.php';
+
+// WordPress constants not available in unit tests.
+if ( ! defined( 'DAY_IN_SECONDS' ) ) {
+    define( 'DAY_IN_SECONDS', 86400 );
+}
